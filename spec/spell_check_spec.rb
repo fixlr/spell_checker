9c7dd7818a8b193e492362f632a0ff969452d5c7
Nathan Fixler <nathan@fixler.org>
2009-12-06 00:04:24 -0500

    Refactored to eliminate rspec custom matchers

--- spec/spell_check_spec.rb
+++ spec/spell_check_spec.rb
@@ -1,62 +1,38 @@
 require File.dirname(__FILE__)+"/../lib/spell_check"
 
-Spec::Matchers.define :be_defined_in do |dict|
-  match do |string|
-    if dict == :default
-      string.extend(SpellCheck).correct?
-    else
-      string.extend(SpellCheck).with_dictionary(dict).correct?
-    end
-  end
-
-  failure_message_for_should do |string|
-    "Expected '#{string}' to be spelled correctly."
-  end
-
-  failure_message_for_should_not do |string|
-    "Expected '#{string}' to be spelled incorrectly."
-  end
-end
-
-Spec::Matchers.define :have_alternatives do |expected|
-  match do |string|
-    string.extend(SpellCheck).alternatives == expected
-  end
-  
-  failure_message_for_should do |string|
-    "Expected '#{string}' to have alternatives [#{expected.join(', ')}] but found [#{string.alternatives.join(', ')}]."
-  end 
-end
-
 describe SpellCheck do
   context "with the default dictionary" do
     it "should not find spelling errors in an empty string" do
-      "".should be_defined_in(:default)
+      spelling("").should be_correct
     end
-  
+      
     it "should know words that are in the word dictionary" do
-      "trademark".should be_defined_in(:default)
+      spelling("trademark").should be_correct
     end
-  
+      
     it "should find spelling errors for words that are not in the word dictionary" do
-      "awesome".should_not be_defined_in(:default)
+      spelling("awesome").should_not be_correct
     end
-
+    
     it "should not suggest alternatives for words that are spelled correctly" do
-      "trademark".should have_alternatives([])
+      spelling("trademark").alternatives.should == []
     end
-  
+      
     it "should suggest alternatives for words that are not spelled correctly" do
-      "trademak".should have_alternatives(['trademark'])
+      spelling("trademak").alternatives.should == ['trademark']
     end
   end
 
   context "with a custom dictionary" do
     it "should know words that are in the custom dictionary" do
-      "awesome".should be_defined_in("spelling is awesome")
+      spelling("awesome").with_dictionary("spelling is awesome").should be_correct
     end
     it "should not know words that are in the default dictionary" do
-      "trademark".should_not be_defined_in("spelling is awesome")
+      spelling("trademark").with_dictionary("spelling is awesome").should_not be_correct
     end
+  end
+
+  def spelling(string)
+    string.extend(SpellCheck)
   end
 end
