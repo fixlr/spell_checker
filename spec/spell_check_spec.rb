480e2138869ba45ac2adf343b02d737fe05c91cb
Nathan Fixler <nathan@fixler.org>
2009-12-06 00:46:24 -0500

    Refactored specs

--- spec/spell_check_spec.rb
+++ spec/spell_check_spec.rb
@@ -1,35 +1,36 @@
 require File.dirname(__FILE__)+"/../lib/spell_check"
 
 describe SpellCheck do
-  context "with the default dictionary" do
-    it "should not find spelling errors in an empty string" do
-      spelling("").should be_correct
-    end
-      
-    it "should know words that are in the word dictionary" do
-      spelling("trademark").should be_correct
-    end
-      
-    it "should find spelling errors for words that are not in the word dictionary" do
-      spelling("awesome").should_not be_correct
-    end
-    
-    it "should not suggest alternatives for words that are spelled correctly" do
-      spelling("trademark").alternatives.should == []
-    end
-      
-    it "should suggest alternatives for words that are not spelled correctly" do
-      spelling("trademak").alternatives.should == ['trademark']
-    end
+  it "should not find spelling errors in an empty string" do
+    spelling("").should be_correct
+  end
+  it "should know when words are spelled correctly" do
+    spelling("trademark").should be_correct
+  end
+  it "should know when words are not spelled correctly" do
+    spelling("awesome").should_not be_correct
+  end
+  it "should not suggest alternatives for words that are spelled correctly" do
+    spelling("trademark").alternatives.should have(0).words
+    spelling("trademark").alternatives.should == []
+  end
+  it "should suggest alternatives for words that are not spelled correctly" do
+    spelling("trademak").alternatives.should have(1).words
+    spelling("trademak").alternatives.should == ['trademark']
   end
 
   context "with a custom dictionary" do
-    it "should know words that are in the custom dictionary" do
+    it "should know words are spelled correctly" do
       spelling("awesome").with_dictionary("spelling is awesome").should be_correct
     end
-    it "should not know words that are in the default dictionary" do
+    it "should know when words are not spelled correctly" do
       spelling("trademark").with_dictionary("spelling is awesome").should_not be_correct
     end
+  end
+  
+  context "an entire sentence" do
+    it "should be able to check an entire sentence"
+    it "should suggest alternatives for each individual word"
   end
 
   def spelling(string)
